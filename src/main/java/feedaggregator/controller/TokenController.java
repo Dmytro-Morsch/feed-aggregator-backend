2219c0a9326d20af868bc46af6d78189d3b19ef2
Dima Morsch <149238209+Dmytro-Morsch@users.noreply.github.com>
2024-08-25 19:32:00 +0300

    fix: Change request param to body

--- src/main/java/feedaggregator/controller/TokenController.java
+++ src/main/java/feedaggregator/controller/TokenController.java
@@ -6,8 +6,10 @@ import feedaggregator.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class TokenController {
@@ -19,11 +21,9 @@ public class TokenController {
     private TokenService tokenService;
 
     @PostMapping("/api/token")
-    public ResponseEntity<String> getToken(@RequestParam String email,
-                                           @RequestParam String password) {
-        User user = userRepository.findByEmail(email);
-        if (!user.getPassword().equals(password)) return ResponseEntity.status(401).build();
-
+    public ResponseEntity<String> getToken(@RequestBody Map<String, String> body) {
+        User user = userRepository.findByEmail(body.get("email"));
+        if (!user.getPassword().equals(body.get("password"))) return ResponseEntity.status(401).build();
         return ResponseEntity.ok(tokenService.createToken(user.getId()));
     }
 }
